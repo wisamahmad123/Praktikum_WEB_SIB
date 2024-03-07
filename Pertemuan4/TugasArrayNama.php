2cd35f284b39a0ab3c78a864d62f1d6a91abc7a0
wisam ahmad risqi armana <thomasthamrin11@gmail.com>
2024-03-07 18:33:55 +0700

    Pertemuan4

--- Pertemuan4/TugasArrayNama.php
+++ Pertemuan4/TugasArrayNama.php
@@ -1,8 +1,31 @@
-looping
+<?php
+$biodata = [
+    'Data1' => [
+        ['Nama', 'Robi Surasih'],
+        ['Alamat', 'Kedungkandang'],
+        ['No HP', '082315523324'],
+        ['Jurusan', 'Jurusan Teknik Mesin'],
+    ],
+    'Data2' => [
+        ['Nama', 'Jisoo Ruby'],
+        ['Alamat', 'Lowokwaru'],
+        ['No HP', '08444213422'],
+        ['Jurusan', 'Jurusan Teknologi Informasi'],
+    ],   
+];
 
-data -> array multidimensi
-2 data 
-nama
-alamat
-no hp
-jurusan
+echo "<img src='img/img1.JPEG'width='200' >";
+echo "<br>";
+foreach ($biodata['Data1'] as $dataDiri) {
+    echo "$dataDiri[0] = $dataDiri[1]";
+    echo "<br>";
+}
+echo "<br>";
+
+echo "<img src='img/img2.JPG' width='200' height='270'>";
+echo "<br>";
+foreach ($biodata['Data2'] as $dataDiri) {
+    echo "$dataDiri[0] = $dataDiri[1]";
+    echo "<br>";
+}
+?>
